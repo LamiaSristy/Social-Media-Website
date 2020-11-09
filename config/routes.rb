dd9ef603460f027e628e559b03e24f407cea2fe4
lamiasristy <lamiasristy@gmail.com>
2020-11-09 13:03:09 +0100

    Added stylesheet.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+  
   devise_for :users
+
+  resources :users, only: [:index, :show]
+  resources :posts, only: [:index, :create] do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
